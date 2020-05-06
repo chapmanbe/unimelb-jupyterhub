dd1df891e591327c431257ab36a4514cce5e1a47
Brian Chapman <chapmanbe@gmail.com>
2020-05-06 15:39:55 +1000

    this code successfully deployed on May 6, 2020

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 # Run openrc.sh to set environment variables,
 # then run playbook
-source ./openrc.sh; ansible-playbook -i inventory/inventory.ini --private-key "$1" playbook.yml $2
+source ./openrc.sh; ansible-playbook -vv -i inventory/inventory.ini --private-key "$1" playbook.yml $2
